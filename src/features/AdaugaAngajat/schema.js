e4dc9da38ef79ff594ec004c9af836d00d574771
adelinazmarandoiu <adelinazmarandoiu@totalsoft.ro>
2022-09-26 14:22:58 +0300

    sta

--- src/features/AdaugaAngajat/schema.js
+++ src/features/AdaugaAngajat/schema.js
@@ -2,26 +2,34 @@ const { gql } = require('apollo-server-koa')
 
 const adaugaAngajatDefs = gql`
   type Angajat {
+    id: Int
     nume: String
     prenume: String
+    email: String
+    parola: String
     dataNasterii: String
     cnp: String
     seriaNumarBuletin: String
     numartelefon: String
     dataAngajarii: String
+    esteAngajatCuActeInRegula: Boolean
     managerId: Int
     echipaId: Int
     salariu: Float
   }
 
   input AngajatInput {
+    id: Int
     nume: String
     prenume: String
+    email: String
+    parola: String
     dataNasterii: String
     cnp: String
     serieNumarBuletin: String
     numardetelefon: String
     dataAngajarii: String
+    esteAngajatCuActeInRegula: Boolean
     managerId: Int
     echipaId: Int
     salariu: Float
