d477564159335087549daeaad50a28af8068918d
Andra Iancu <95624161+xAndruti@users.noreply.github.com>
2022-09-29 11:58:29 +0300

    gataa

--- src/features/promovare/schema.js
+++ src/features/promovare/schema.js
@@ -13,18 +13,9 @@ const updateManagerIdEchipaIdDefs = gql`
     nume: String
     prenume: String
     email: String
-    # parola: String
-    # dataAngajarii: String
-    # dataNasterii: String
     cnp: String
-    # seriaNumarBuletin: String
-    # numarTelefon: String
-    # poza: String
-    ManagerId: Int
     echipa: Int
-    # salariu: String
-    # esteAdmin: Boolean
-    # esteAngajatCuActeInRegula: Boolen
+    ManagerId: Int
   }
   extend type Mutation {
     updateManagerIdEchipaId(input: [AngajatInput]): String
